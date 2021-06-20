3bfb53e7c8b95bc9cf91bfcb7b50a9129d98cd2d
william walsh <william.walsh@digitary.net>
2021-06-20 21:45:06 +0100

    Initial Commit & add httpclient code to test sample website.

---
+++ src/test/java/service_testing/XmlServiceTest.java
@@ -0,0 +1,26 @@
+package service_testing;
+
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class XmlServiceTest {
+    private static final String SAMPLE_URL = "https://en.wikipedia.org/wiki/Hokusai";
+
+    @Test
+    public void testHttpGetXml() throws IOException {
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpResponse response = client.execute(new HttpGet(SAMPLE_URL));
+        int statusCode = response.getStatusLine().getStatusCode();
+
+        assertEquals("Status code is not OK - HTTP200", HttpStatus.SC_OK, statusCode);
+    }
+}
