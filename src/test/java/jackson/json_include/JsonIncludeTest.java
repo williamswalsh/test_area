5f87af0a41459daa819a549459975d7ad2e6064f
william walsh <william.walsh@digitary.net>
2022-08-02 16:19:41 +0100

    Add assert statements to tests, in order to test the outcome of the jackson code.

--- src/test/java/jackson/json_include/JsonIncludeTest.java
+++ src/test/java/jackson/json_include/JsonIncludeTest.java
@@ -11,6 +11,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static junit.framework.TestCase.assertEquals;
+
 public class JsonIncludeTest {
 
     @Test
@@ -18,7 +20,15 @@ public class JsonIncludeTest {
         Student student = new Student("liam", "mail", "person@domain.com", "08723456789",
                 Arrays.asList("Maths", "Java", "Docker"));
         ObjectWriter writer = new ObjectMapper().writer().withDefaultPrettyPrinter();
-        System.out.println(writer.writeValueAsString(student));
+        String expected = "{\n"
+                + "  \"firstName\" : \"liam\",\n"
+                + "  \"lastName\" : \"mail\",\n"
+                + "  \"email\" : \"person@domain.com\",\n"
+                + "  \"mobileNumber\" : \"08723456789\",\n"
+                + "  \"courses\" : [ \"Maths\", \"Java\", \"Docker\" ]\n"
+                + "}";
+        String actual = writer.writeValueAsString(student);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -26,7 +36,30 @@ public class JsonIncludeTest {
         Student student = new Student("liam", "mail", "person@domain.com", "08723456789",
                 Collections.emptyList());
         ObjectWriter writer = new ObjectMapper().writer().withDefaultPrettyPrinter();
-        System.out.println(writer.writeValueAsString(student));
+        String actual = writer.writeValueAsString(student);
+        String expected = "{\n"
+                + "  \"firstName\" : \"liam\",\n"
+                + "  \"lastName\" : \"mail\",\n"
+                + "  \"email\" : \"person@domain.com\",\n"
+                + "  \"mobileNumber\" : \"08723456789\",\n"
+                + "  \"courses\" : [ ]\n"
+                + "}";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void nullListTest() throws JsonProcessingException {
+        Student student = new Student("liam", "mail", "person@domain.com", "08723456789",
+                null);
+        ObjectWriter writer = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String expected = "{\n"
+                + "  \"firstName\" : \"liam\",\n"
+                + "  \"lastName\" : \"mail\",\n"
+                + "  \"email\" : \"person@domain.com\",\n"
+                + "  \"mobileNumber\" : \"08723456789\"\n"
+                + "}";
+        String actual = writer.writeValueAsString(student);
+        assertEquals(expected, actual);
     }
 }
 
