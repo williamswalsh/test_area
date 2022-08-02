b97c13ab8094f8c81641d919f8b3980e728eea4f
william walsh <william.walsh@digitary.net>
2022-08-02 16:12:43 +0100

    Added jackson JsonInclude examples. Remove cached file inside .idea folder.

---
+++ src/test/java/jackson/json_include/JsonIncludeTest.java
@@ -0,0 +1,42 @@
+package jackson.json_include;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import lombok.Data;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class JsonIncludeTest {
+
+    @Test
+    public void listWithElementsTest() throws JsonProcessingException {
+        Student student = new Student("liam", "mail", "person@domain.com", "08723456789",
+                Arrays.asList("Maths", "Java", "Docker"));
+        ObjectWriter writer = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        System.out.println(writer.writeValueAsString(student));
+    }
+
+    @Test
+    public void emptyListTest() throws JsonProcessingException {
+        Student student = new Student("liam", "mail", "person@domain.com", "08723456789",
+                Collections.emptyList());
+        ObjectWriter writer = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        System.out.println(writer.writeValueAsString(student));
+    }
+}
+
+// It includes all non_null inputs, i.e. it won't include null
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+class Student {
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final String mobileNumber;
+    private final List<String> courses;
+}
