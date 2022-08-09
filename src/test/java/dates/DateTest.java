1d35c3459ac217f35c052812e2bbd9bb9d0c0834
william walsh <william.walsh@digitary.net>
2022-08-09 10:08:36 +0100

    Add date constructor toString format test.

---
+++ src/test/java/dates/DateTest.java
@@ -0,0 +1,13 @@
+package dates;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+public class DateTest {
+
+    @Test
+    public void dateConstructorTest() {
+        System.out.println(new Date());
+    }
+}
