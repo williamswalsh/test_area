36b76641b3d9f5f9f6a80d5f28c8ce866f71f114
william walsh <william.walsh@digitary.net>
2022-08-10 13:01:34 +0100

    Adding shelved changes to GIT.

--- src/test/java/intellij/IntellijTesting.java
+++ src/test/java/intellij/IntellijTesting.java
@@ -1,8 +1,10 @@
 package intellij;
 
+import org.junit.Test;
+
 public class IntellijTesting {
-
-
-
-
+    @Test
+    public void thisIsAnExperimentalTest() {
+        System.out.println("This is an experimental test, that I am unlikely to commit.");
+    }
 }
