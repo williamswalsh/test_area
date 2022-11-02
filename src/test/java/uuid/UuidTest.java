07b38bb916f577479f495a394e3fcf6a395e35de
William Walsh <william.walsh@digitary.net>
2022-11-02 11:52:30 +0000

    Add UUID pattern tester.

--- src/test/java/uuid/UuidTest.java
+++ src/test/java/uuid/UuidTest.java
@@ -3,9 +3,12 @@ package uuid;
 import org.junit.Test;
 
 import java.util.UUID;
+import java.util.regex.Pattern;
+
+import static junit.framework.TestCase.assertTrue;
 
 public class UuidTest {
-
+    private static final String UUID_PATTERN = "[0-9a-zA-Z]{8}-[0-9a-zA-Z]{4}-[0-9a-zA-Z]{4}-[0-9a-zA-Z]{4}-[0-9a-zA-Z]{12}";
     @Test
     public void uuidBlank() throws Exception {
         UUID uuid = null;
@@ -18,5 +21,11 @@ public class UuidTest {
         if(uuid2.equals("")) {
             throw new Exception("isBlank - testing exception");
         }
+    }
+
+    @Test
+    public void checkUuidAgainstPattern() {
+        UUID productId = UUID.randomUUID();
+        assertTrue(Pattern.matches(UUID_PATTERN, productId.toString()));
     }
 }
