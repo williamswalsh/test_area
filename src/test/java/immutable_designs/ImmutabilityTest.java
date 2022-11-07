cc63ff4df4beac426eecdad3959c520ed3201969
William Walsh <william.walsh@digitary.net>
2022-11-07 15:51:42 +0000

    Add simple stream demo for Deepak.

--- src/test/java/immutable_designs/ImmutabilityTest.java
+++ src/test/java/immutable_designs/ImmutabilityTest.java
@@ -6,11 +6,11 @@ import org.junit.Test;
  * @author William Walsh
  * @version 02 Nov 2022
  */
-public class ImmutabilityTest {
-    @Test
-    public void testImmutabilityOfHiddenInputsObject() {
-        HiddenInputs hidden = new HiddenInputs("share_id_sample", "doc_id_sample");
-        hidden.docId = "s";
-
-    }
-}
+// public class ImmutabilityTest {
+//     @Test
+//     public void testImmutabilityOfHiddenInputsObject() {
+//         HiddenInputs hidden = new HiddenInputs("share_id_sample", "doc_id_sample");
+//         hidden.docId = "s";
+//
+//     }
+// }
--- src/test/java/stream/StreamValueTest.java
+++ src/test/java/stream/StreamValueTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamValueTest {
 
@@ -19,5 +20,20 @@ public class StreamValueTest {
         List<String> emptyList = new ArrayList<>();
         // emptyList.stream().forEach(System.out::println);
         emptyList.stream().map((s -> s + " suffix"));
+    }
+
+
+    @Test
+    public void streamsDemo() {
+        List<String> names = new ArrayList<>();
+        names.add("alice");
+        names.add("bob");
+        names.add("claire");
+
+        List<String> upperCaseNames = names.stream()
+                .map(name -> name.toUpperCase())
+                .collect(Collectors.toList());
+
+    //            .forEach(name -> System.out.println(name))
     }
 }
