88ab13264b7771b7ebe996a4a9cc132684469e2e
William Walsh <william.stephen.walsh@gmail.com>
2024-04-16 12:01:57 +1000

    Add map tests.

--- src/test/java/hash/HashMapTest.java
+++ src/test/java/hash/HashMapTest.java
@@ -15,6 +15,23 @@ public class HashMapTest {
         intPresenceMap.put(null, null);
         intPresenceMap.put(7, null);
         intPresenceMap.put(null, true);
+    }
 
+    @Test
+    public void unorderedTest() {
+        Map<Integer, Boolean> map = new HashMap<>();
+
+        map.put(1, true);
+        map.put(2, true);
+        map.put(3, true);
+        map.put(4, true);
+        map.put(5, true);
+        map.put(6, true);
+        map.put(7, true);
+
+//        Keys printed in order??
+        for(int key: map.keySet()) {
+            System.out.println(key + ": " + map.get(key));
+        }
     }
 }
