d873af2e77d57e2ca8ab36d042b358e7dbb0ae87
William Walsh <william.stephen.walsh@gmail.com>
2024-06-28 06:15:29 +1000

    Add longest substring code.

---
+++ src/test/java/tiktok_interview/LongestSubstring.java
@@ -0,0 +1,63 @@
+package tiktok_interview;
+
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class LongestSubstring {
+    /*
+    abcdaghju
+        s
+            e
+    */
+    private static int longestSubString(String str) {
+        Map<Character, Integer> present = new HashMap<>();
+        int start = 0;
+        int end = 0;
+        char[] chars = str.toCharArray();
+        char currChar;
+
+        while(end < str.length()) {
+            currChar = chars[end];
+            if (present.containsKey(currChar)) {
+                // move start pointer to end
+                start = present.get(currChar) + 1;
+                // reset
+                present.clear();
+            } else {
+                // Add char to list
+                present.put(currChar, end);
+                // Increment ptr
+                end++;
+            }
+        }
+        return end - start;
+    }
+
+    @Test
+    public void testLongestSubString() {
+        assertEquals(8, longestSubString("abcdaghju"));
+    }
+
+    @Test
+    public void testLongestSubStringSimple() {
+        assertEquals(3, longestSubString("abc"));
+    }
+
+    @Test
+    public void testLongestSubStringSingleChar() {
+        assertEquals(1, longestSubString("a"));
+    }
+
+    @Test
+    public void testLongestSubStringTwoChar() {
+        assertEquals(2, longestSubString("ab"));
+    }
+
+    @Test
+    public void testLongestSubStringWithDuplicateAtEnd() {
+        assertEquals(2, longestSubString("aba"));
+    }
+}
