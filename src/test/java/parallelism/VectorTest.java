8d443ee2e9b242212fc6afe62faaad59e70d7ad4
William Walsh <william.stephen.walsh@gmail.com>
2025-04-26 23:12:07 +1000

    Add interesting vector vs arraylist performance and correctness comparison.

--- src/test/java/parallelism/VectorTest.java
+++ src/test/java/parallelism/VectorTest.java
@@ -20,15 +20,26 @@ public class VectorTest {
         System.out.println(ints);
     }
 
-//    Arraylist is not threadsafe - asserting size test fails
-//    vector is synchronised
+
+//    Arraylist is not threadsafe
+//    using single threaded executor
+//    asserting size test FAILS!!!
     @Test
-    void checkArrayListSizeIncreaseTest() throws InterruptedException {
-        int totalNumOfTasks = 100000;
+    void checkArrayListSizeIncreaseFailureTest() throws InterruptedException {
+        int totalNumOfTasks = 10000;
         ArrayList<Integer> ints = new ArrayList<>();
 
-        Runnable increaseSizeTask = () -> ints.add(1);
-        ExecutorService e = Executors.newFixedThreadPool(10);
+        Runnable increaseSizeTask = () -> {
+            ints.add(1);
+//            Adding a wait makes difference larger
+//            try {
+//                Thread.sleep(1);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+        };
+//        Many threads - result is worse
+        ExecutorService e = Executors.newFixedThreadPool(1000);
 
         for (int tasks = 0; tasks < totalNumOfTasks; tasks++) {
             e.execute(increaseSizeTask);
@@ -36,7 +47,7 @@ public class VectorTest {
 
 //    Necessary to shut down executor service and wait for the tasks to complete
         e.shutdown();
-        e.awaitTermination(10, TimeUnit.SECONDS);
+        e.awaitTermination(999999, TimeUnit.SECONDS);
 
         int expectedSize = totalNumOfTasks;
 //    This test fails as the final size is nothing like the expected size totalNumOfTasks
@@ -45,16 +56,25 @@ public class VectorTest {
         assertEquals(expectedSize, ints.size());
     }
 
-//    vector is threadsafe in some cases - asserting size test succeeds
+//    Arraylist is not threadsafe
+//    using single threaded executor
+//    asserting size test SUCCEEDS!!!
     @Test
-    void checkVectorSizeIncreaseTest() throws InterruptedException {
-        int totalNumOfTasks = 100000;
+    void checkArrayListSizeIncreaseTest() throws InterruptedException {
+        int totalNumOfTasks = 100;
+        ArrayList<Integer> ints = new ArrayList<>();
 
-//        Only change is data structure ArrayList -> Vector
-        Vector<Integer> ints = new Vector<>();
-
-        Runnable increaseSizeTask = () -> ints.add(1);
-        ExecutorService e = Executors.newFixedThreadPool(10);
+        Runnable increaseSizeTask = () -> {
+            ints.add(1);
+            try {
+                // Waits will
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        };
+        // This class can't be reconfigured to have a greater number of threads in thread pool.(newFixedThreadPoolSize
+        ExecutorService e = Executors.newSingleThreadExecutor();
 
         for (int tasks = 0; tasks < totalNumOfTasks; tasks++) {
             e.execute(increaseSizeTask);
@@ -62,7 +82,51 @@ public class VectorTest {
 
 //    Necessary to shut down executor service and wait for the tasks to complete
         e.shutdown();
-        e.awaitTermination(10, TimeUnit.SECONDS);
+        e.awaitTermination(9999999, TimeUnit.SECONDS);
+
+        int expectedSize = totalNumOfTasks;
+//    This test fails as the final size is nothing like the expected size totalNumOfTasks
+//    Expected :100000
+//    Actual   :99936 -> off by 64 in this case
+        assertEquals(expectedSize, ints.size());
+    }
+
+//    vector is threadsafe in some cases
+//    Using fixed thread pool size>1
+//    asserting size test succeeds
+    @Test
+    void checkVectorSizeIncreaseTest() throws InterruptedException {
+        int totalNumOfTasks = 1000;
+
+//        Only change is data structure ArrayList -> Vector
+        Vector<Integer> ints = new Vector<>();
+
+//        Runnable increaseSizeTask = () -> ints.add(1);
+        Runnable increaseSizeTask = () -> {
+            ints.add(1);
+            try {
+                // Waits will
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        };
+
+//        N.B.
+//        If single threaded - single thread waits for first task then second
+//        Test will take 10seconds+
+//        If 100 threads - threads "wait in parallel"
+//        Test takes less than 1second :-) Cool
+//        Playing with the size of the thread pool totally affects the runtime
+        ExecutorService e = Executors.newFixedThreadPool(100);
+
+        for (int tasks = 0; tasks < totalNumOfTasks; tasks++) {
+            e.execute(increaseSizeTask);
+        }
+
+//    Necessary to shut down executor service and wait for the tasks to complete
+        e.shutdown();
+        e.awaitTermination(999999, TimeUnit.SECONDS);
 
         int expectedSize = totalNumOfTasks;
 //    This test always succeeds, Cool :-)
